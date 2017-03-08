080e22984f9d454c41fdfd5ec57bef118e02efd3
Roberto Arriola <arriola.rock97@gmail.com>
2017-03-08 14:03:02 -0600

    Creacion y Cambios
    *Se añadio una label y un combobox
    *Creacion de la Clase Usuario

---
+++ Bases_RM/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bases_RM
+{
+    class Usuario
+    {
+        public String Nombre = "", Clave = "",Cadena_Pedido="",Cadena_Seguridad="", Cadena_Cliente="", Cadena_Trabajador="";
+        public Usuario(String nombre, String clave, String seguridad, String cliente, String trabajador, String pedido)
+        {
+            this.Nombre = nombre;
+            this.Clave = clave;
+            this.Cadena_Seguridad = seguridad;
+            this.Cadena_Cliente = cliente;
+            this.Cadena_Trabajador = trabajador;
+            this.Cadena_Pedido = pedido;
+        }
+
+    }
+}
