3c715dd3a484c634208cd569bd447442d28cfa66
Roberto Arriola <arriola.rock97@gmail.com>
2017-03-10 12:18:01 -0600

    *Inclusion de metodo obtener sucursales en la Clase de conexion *Implementacion del metodo obtenet sucursales

--- Bases_RM/AbonoDeuda.Designer.cs
+++ Bases_RM/AbonoDeuda.Designer.cs
@@ -137,6 +137,7 @@
             this.btnCancel.TabIndex = 10;
             this.btnCancel.Text = "Cancelar";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnguar
             // 
