3e79eef95451a2da4bb42eb4bb53f66a87f272f1
robertoar14 <arriola.rock97@gmail.com>
2017-04-29 14:38:25 -0600

    Cambio de IP del servidor

--- Bases_RM/Conexion_DB.cs
+++ Bases_RM/Conexion_DB.cs
@@ -16,7 +16,7 @@ namespace Bases_RM
         private MySqlDataReader Variable_Lectura;//Variable que se usa para leer datos
         private MySqlCommand comando;//Comando SQL para hacer las consultas
         public Conexion_DB(){
-            Constructor_Conexion.Server = "25.73.161.33";//Direccion IP del servidor
+            Constructor_Conexion.Server = "25.3.39.210";//Direccion IP del servidor
             Constructor_Conexion.UserID = "root";//Ususario de la base de datos
             Constructor_Conexion.Password = "@Sistemas2017";//Contraseña para la base de datos 
             Constructor_Conexion.Database = "rm_db";//Nombre de la base de datos
